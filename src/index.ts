57f453dfe3a5984150e3550ada8e4c76c3667019
Noah Mulfinger <noah@Noahs-MBP.fios-router.home>
2016-11-12 02:22:33 -0800

    Added typo checking for single file

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,57 @@
 #! /usr/bin/env node
+import { Observable } from 'rxjs';
 import { argv } from 'yargs';
 import * as fs from 'graceful-fs';
-const validWords = require('../typoconfig.json').validWords;
+import * as readdir from 'recursive-readdir';
 
-interface WordDictionary {
-    [index: string]: boolean;
+interface WordData {
+  file: string;
+  line: number;
+  word: string;
 }
 
-let dict: WordDictionary = {};
+function fileFilter(file, stats) {
+  return !(stats.isDirectory() || file.endsWith('.html'));
+}
+
+function loadWordList(fileName: string): Observable<string[]> {
+  console.log('Loading valid word list...')
+    return Observable.bindNodeCallback(fs.readFile)(fileName)
+        .map(data => data.toString().split('\n').filter(str => str != ''))
+}
+
+function readFile(fileName: string): Observable<WordData[]> {
+    console.log('Loading file ' + fileName + '...')
+    return Observable.bindNodeCallback(fs.readFile)(fileName)
+        .map(data => data.toString().toLowerCase().replace(/<\/?[^>]*>/g, " "))
+        .map(data => data.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g,"").split('\n'))
+        .map(lines => {
+            let words = [];
+            lines.forEach((line, lineNumber) => {
+                let arr = line.match(/\w*[^\s]/g)
+                if (arr == null) { return; }
+                arr.forEach((word) => {
+                    words.push({file: fileName, line: lineNumber, word: word});
+                })
+            })
+            return words;
+        })
+}
+
+// readdir('/Users/noah/projects/personal-site/', [fileFilter], (err, files) => {
+//   console.log(files);
+// });
 
 
 
-fs.readFile(argv._[0], 'UTF-8', (err, data) => {
-  if (err) {
-    console.log('Error reading file: ' + err);
-    return;
-  }
-  let lines = data.split('\n');
-  let words = [];
-  for (let i = 0; i < lines.length; i++) {
-    words = data.replace(/<\/?[^>]*>/g, " ")
-    .split(" ")
-    .filter((str) => str != '')
-    .map((str) => str.toLowerCase())
-  }
-});
+Observable.zip(loadWordList(__dirname + '/dict.txt'), readFile(argv._[0])).subscribe(data => {
+    let validWords = data[0];
+    let fileData = data[1];
+
+    console.log('Misspelled words:')
+    fileData.forEach((wordData) => {
+      if (validWords.indexOf(wordData.word) == -1) {
+        console.log(wordData.file + ' [line ' + wordData.line + ']: ' + wordData.word);
+      }
+    })
+})
